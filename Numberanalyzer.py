846073e262d53ceb1e274a18249bc0100aee6821
mohammedmansoorai26-web <mohammedmansoor.ai26@bmsce.ac.in>
2026-08-29 12:25:22 +0530

    Create Numberanalyzer.py

---
+++ Numberanalyzer.py
@@ -0,0 +1,16 @@
+number = int(input("Enter number:",))
+
+#Checking even
+if number%2 == 0 :
+	print("Even")
+else:
+	print("Odd")
+	
+#Checking type
+if number > 0 :
+	print("Positive")
+elif number < 0 :
+	print("Negative")
+else:
+	print("Zero")
+	
