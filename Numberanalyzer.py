72e759b0d39d308d3c5f2521c41aff07da529d22
mohammedmansoorai26-web <mohammedmansoor.ai26@bmsce.ac.in>
2026-08-29 12:48:49 +0530

    Update Numberanalyzer.py

--- Numberanalyzer.py
+++ Numberanalyzer.py
@@ -1,16 +1,43 @@
-number = int(input("Enter number:",))
+first = int(input("Enter first number :",))
+second = int(input("Enter Second number:",))
 
-#Checking even
-if number%2 == 0 :
+#Type of first
+print("---First number---")
+if first%2 == 0 :
 	print("Even")
 else:
 	print("Odd")
 	
-#Checking type
-if number > 0 :
-	print("Positive")
-elif number < 0 :
+if first> 0 :
+	print('Positive')
+elif first<0 :
 	print("Negative")
 else:
 	print("Zero")
+print()
+#Type of first
+print("---Second number---")
+if second%2 == 0 :
+	print("Even")
+else:
+	print("Odd")
+	
+if second> 0 :
+	print('Positive')
+elif second<0 :
+	print("Negative")
+else:
+	print("Zero")	
+print()	
+#Checking largest/smallest
+print("---Comparison---")
+if first > second:
+	print("Largest =",first)
+	print("Smallest = ",second)
+elif second > first:
+	print("Largest =",second)
+	print("Smallest = ",first)
+else:
+	print("Both are equal")
+	
 	
